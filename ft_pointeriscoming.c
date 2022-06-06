cd9946874991c68dcf2dcbb497c5f1945ebbb721
Giuliano Dominici <gdominic@student.42barcelona.com>
2022-06-06 21:12:38 +0200

    pointer next

---
+++ ft_pointeriscoming.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pointeriscoming.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdominic <gdominic@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/06 19:52:52 by gdominic          #+#    #+#             */
+/*   Updated: 2022/06/06 21:10:37 by gdominic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_printf.h"
+
+int	ft_pointeriscoming(unsigned long long strike, int nextbase)
+{
+/*	int	 i;
+	char  *convert; //storage for base exadecimals
+	char  *newbuff;
+*/	nextbase++;
+	printf("%llu\n", strike);
+	return (1);
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gdominic <gdominic@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 20:13:38 by gdominic          #+#    #+#             */
-/*   Updated: 2022/06/06 18:28:16 by gdominic         ###   ########.fr       */
+/*   Updated: 2022/06/06 21:06:36 by gdominic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
@@ -57,9 +57,11 @@ static	int	ft_splitter(char const *src, va_list ptr)
 		return (count += ft_convertx(va_arg(ptr, unsigned int), 16, 0));
 	if (*src == 'u')
 		return (count += ft_putnbr_u(va_arg(ptr, unsigned int), count));
+	if (*src == 'p')
+		return (count += ft_pointeriscoming(va_arg(ptr, unsigned long long), 16));
 	return (count);
 }
-/*
+
 int	main(void)
 {
 	char		  *s;
@@ -76,67 +78,12 @@ int	main(void)
 	d = -212;
 	c = 'h';
 	u = 10;
-//	re = ft_printf("Mi dice %s, %d, %c, %x, %u, %%\n", s, d, c, -8, u);
-//	re2 = printf("Mi dice %s, %d, %c, %x, %u, %%\n", s, d, c, x, u);
-	re = ft_printf("%x ", 0);
-	re2 = printf("%x ", 0);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", -10);
-	re2 = printf("%x ", -10);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", -200000);
-	re2 = printf("%x ", -200000);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", -6000023);
-	re2 = printf("%x ", -6000023);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", 10);
-	re2 = printf("%x ", 10);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", 10000);
-	re2 = printf("%x ", 10000);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", 100023);
-	re2 = printf("%x ", 100023);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", 0xabcdef);
-	re2 = printf("%x ", 0xabcdef);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", 0x7fedcba1);
-	re2 = printf("%x ", 0x7fedcba1);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", 2147483647);
-	re2 = printf("%x ", 2147483647);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", -2147483648);
-	re2 = printf("%lx ", -2147483648);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x ", 4294967295);
-	re2 = printf("%lx ", 4294967295);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-	re = ft_printf("%x%xx%x ", 1, 2, -3);
-	re2 = printf("%x%xx%x ", 1, 2, -3);
-	printf(" re is: %d, re2 is %d\n", re, re2);
-
-	printf("%x", -10);
-	printf("%x", -200000);
-	printf("%x", -6000023);
-	printf("%x", 10);
-	printf("%x", 10000);
-	printf("%x", 100023);
-	printf("%x", 0xabcdef);
-	printf("%x", 0x7fedcba1);
-	printf("%x", 2147483647);
-	printf("%x", -2147483648);
-	printf("%x", 4294967295);
-	printf("dgs%xxx", 10);
-	printf("%x%xx%x", 1, 2, -3);
-	re2 = printf("%x ", -10);
-
+	re = ft_printf("Mi dice %s, %d, %c, %x, %u, %%, %p\n", s, d, c, -8, u, s);
+	re2 = printf("Mi dice %s, %d, %c, %x, %u, %%, %p\n", s, d, c, x, u, s);
 	printf("re is: %d, re2 is %d\n", re, re2);
 //	int k = ft_printf(" %x ", -42);
 //	printf("k = %d\n", k);
 //	int j = printf(" %x ", -42);
 //	printf("j = %d\n", j);
 	return (0);
-}*/
+}
