de8e922327d5928c71bffae42c9b8269ce6f3e03
Giuliano Dominici <gdominic@student.42barcelona.com>
2022-06-06 19:42:30 +0200

    hexadecimals done

--- ft_convertx.c
+++ ft_convertx.c
@@ -6,24 +6,39 @@
 /*   By: gdominic <gdominic@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 02:46:03 by gdominic          #+#    #+#             */
-/*   Updated: 2022/06/05 20:31:37 by gdominic         ###   ########.fr       */
+/*   Updated: 2022/06/06 19:39:41 by gdominic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
+static int	ft_realconvert(char *buffer, int i)
+{
+	int	counter;
+
+	counter = 0;
+	buffer[i] = '\0';
+	counter += ft_strlen(buffer);
+	while ((i - 1) >= 0)
+	{
+		ft_putchar(buffer[i - 1]);
+		i--;
+	}
+	free(buffer);
+	return (counter);
+}
+
 int	ft_convertx(unsigned int num, int base, int mode)
 {
-	int		counter;
+	int		i;
 	char	*symbols;
 	char	*buffer;
-	char	*ptr;
 
+	i = 0;
 	if (num == 0)
 	{
 		write(1, "0", 1);
 		return (1);
 	}
-	counter = 0;
 	buffer = (char *)malloc(sizeof(char) * 9);
 	if (!buffer)
 		return (0);
@@ -31,21 +46,11 @@ int	ft_convertx(unsigned int num, int base, int mode)
 		symbols = "0123456789ABCDEF";
 	else
 		symbols = "0123456789abcdef";
-	ptr = &buffer[8];
-	free(buffer);
-	*ptr = 0;
 	while (num != 0)
 	{
-		*ptr = symbols[num % base];
+		buffer[i] = symbols[num % base];
 		num /= base;
-		ptr++;
-		counter++;
+		i++;
 	}
-	*ptr = '\0';
-	while (*--ptr >= 0)
-	{
-		if (*ptr != 0)
-			ft_putchar(*ptr);
-	}
-	return (counter);
+	return (ft_realconvert(buffer, i));
 }
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gdominic <gdominic@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 20:13:38 by gdominic          #+#    #+#             */
-/*   Updated: 2022/06/05 20:33:01 by gdominic         ###   ########.fr       */
+/*   Updated: 2022/06/06 18:28:16 by gdominic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
@@ -59,7 +59,7 @@ static	int	ft_splitter(char const *src, va_list ptr)
 		return (count += ft_putnbr_u(va_arg(ptr, unsigned int), count));
 	return (count);
 }
-
+/*
 int	main(void)
 {
 	char		  *s;
@@ -71,17 +71,72 @@ int	main(void)
 	int re2;
 	//system("clear");
 	s = "ciao figo";
-	x = -42;
+	x = -10;
 	s = "ciao vecio";
 	d = -212;
 	c = 'h';
 	u = 10;
-	re = ft_printf("Mi dice %s, %d, %c, %x, %u, %%\n", s, d, c, -42, u);
-	re2 = printf("Mi dice %s, %d, %c, %x, %u, %%\n", s, d, c, x, u);
+//	re = ft_printf("Mi dice %s, %d, %c, %x, %u, %%\n", s, d, c, -8, u);
+//	re2 = printf("Mi dice %s, %d, %c, %x, %u, %%\n", s, d, c, x, u);
+	re = ft_printf("%x ", 0);
+	re2 = printf("%x ", 0);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", -10);
+	re2 = printf("%x ", -10);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", -200000);
+	re2 = printf("%x ", -200000);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", -6000023);
+	re2 = printf("%x ", -6000023);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", 10);
+	re2 = printf("%x ", 10);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", 10000);
+	re2 = printf("%x ", 10000);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", 100023);
+	re2 = printf("%x ", 100023);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", 0xabcdef);
+	re2 = printf("%x ", 0xabcdef);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", 0x7fedcba1);
+	re2 = printf("%x ", 0x7fedcba1);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", 2147483647);
+	re2 = printf("%x ", 2147483647);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", -2147483648);
+	re2 = printf("%lx ", -2147483648);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x ", 4294967295);
+	re2 = printf("%lx ", 4294967295);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+	re = ft_printf("%x%xx%x ", 1, 2, -3);
+	re2 = printf("%x%xx%x ", 1, 2, -3);
+	printf(" re is: %d, re2 is %d\n", re, re2);
+
+	printf("%x", -10);
+	printf("%x", -200000);
+	printf("%x", -6000023);
+	printf("%x", 10);
+	printf("%x", 10000);
+	printf("%x", 100023);
+	printf("%x", 0xabcdef);
+	printf("%x", 0x7fedcba1);
+	printf("%x", 2147483647);
+	printf("%x", -2147483648);
+	printf("%x", 4294967295);
+	printf("dgs%xxx", 10);
+	printf("%x%xx%x", 1, 2, -3);
+	re2 = printf("%x ", -10);
+
 	printf("re is: %d, re2 is %d\n", re, re2);
 //	int k = ft_printf(" %x ", -42);
 //	printf("k = %d\n", k);
 //	int j = printf(" %x ", -42);
 //	printf("j = %d\n", j);
 	return (0);
-}
+}*/
